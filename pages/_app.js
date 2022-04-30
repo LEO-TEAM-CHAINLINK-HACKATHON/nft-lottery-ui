34b562ccde1f16fa0aa930526b2260bbc6a75d47
leo <leonard.ll.l81@gmail.com>
2022-04-30 13:14:46 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-
+import '../styles/globals.scss'
+import '../styles/custom.scss'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
