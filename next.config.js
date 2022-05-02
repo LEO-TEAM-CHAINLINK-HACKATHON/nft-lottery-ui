1b2b792df4ed6a279e663081a4e5a84820070c4f
leo <leonard.ll.l81@gmail.com>
2022-05-02 17:16:30 +0200

    main

--- next.config.js
+++ next.config.js
@@ -14,10 +14,13 @@ sassOptions: {
   includePaths: [path.join(__dirname, 'styles')],
 },
 images: {
-  formats: ['image/avif', 'image/webp'],
-  deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  domains: ['wild-dream-9169.on.fleek.co'],
+  
+     disableStaticImages: true,
+     
+  // formats: ['image/avif', 'image/webp'],
+  // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  // domains: ['wild-dream-9169.on.fleek.co'],
 },
   module: {
     rules: [
