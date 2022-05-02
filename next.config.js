31e02e76f1670780e8e3d657032a07167faa8251
leo <leonard.ll.l81@gmail.com>
2022-05-02 13:10:33 +0200

    main

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
       '/': {page:'/'}  
     };
 },
+sassOptions: {
+  includePaths: [path.join(__dirname, 'styles')],
+},
+images: {
+  formats: ['image/avif', 'image/webp'],
+  deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  domains: ['realestate-eosin-xi.vercel.app','img3.idealista.com','unicohogar.com'],
+},
   module: {
     rules: [
       //...
