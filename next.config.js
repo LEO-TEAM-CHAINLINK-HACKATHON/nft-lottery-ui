aa5af05043438589c4e3c2eacf07548fecde29b3
leo <leonard.ll.l81@gmail.com>
2022-05-02 17:45:51 +0200

    main

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ images: {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: [
           {
-            loader: ['file-loader'],
+            //loader: ['file-loader'],
             options: {
               name: 'images/[hash]-[name].[ext]',
             },
