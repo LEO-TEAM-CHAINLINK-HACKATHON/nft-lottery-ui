45fe3a50b72b60caa96aac4a320b3cbc34e35dbd
leo <leonard.ll.l81@gmail.com>
2022-05-08 11:50:38 +0200

    main

--- pages/src/components/Carousel.js
+++ pages/src/components/Carousel.js
@@ -1,5 +1,8 @@
 import Image from "next/image"
 import bike from "../../public/SLV4-01-Model-Preview-1050x650.png.svg"
+import art1 from "../../public/image2vector.svg"
+import art2 from "../../public/image2vector (1).svg"
+import art3 from "../../public/image2vector (3).svg"
 import Carousel from "react-bootstrap/Carousel"
 
 const myLoader = ({ src }) => {
@@ -26,11 +29,12 @@ const myLoader = ({ src }) => {
   </Carousel.Item>
   <Carousel.Item interval={500}>
   <Image
+  fill="white"
   alt="prize"
    className="d-block w-100"
    layout="responsive"
    loader={myLoader}
-   src={bike}
+   src={art1}
    height={500}
    width={750} />
     <Carousel.Caption>
@@ -40,11 +44,29 @@ const myLoader = ({ src }) => {
   </Carousel.Item>
   <Carousel.Item>
   <Image
+    fill="white"
+
    alt="prize"
    className="d-block w-100"
    layout="responsive"
    loader={myLoader}
-   src={bike}
+   src={art2}
+   height={500}
+   width={750} />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+  <Image
+    fill="white"
+
+   alt="prize"
+   className="d-block w-100"
+   layout="responsive"
+   loader={myLoader}
+   src={art3}
    height={500}
    width={750} />
     <Carousel.Caption>
