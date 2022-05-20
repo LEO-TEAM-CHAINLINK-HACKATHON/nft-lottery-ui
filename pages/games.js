c77d915053e37fbfdbbd819f7ee441f3ed196d9b
leo <leonard.ll.l81@gmail.com>
2022-05-20 08:41:14 +0200

    💄 games started + svgs

--- pages/games.js
+++ pages/games.js
@@ -1,8 +1,22 @@
+import Image from "next/image"
+import { aS, deckBack } from "./src/helpers/cards"
+
+const myLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`
+  };
 const Games = (second) => {
     return (
         <div className="section">
             <div className="container">
-                <h1>Games</h1>
+                <h1 className="text-white m-4">Games</h1>
+            </div>
+            <div className="section">
+                <div className="container">
+                <Image src={deckBack} alt="card" height={320} width={220} layout="fixed" loader={myLoader}/>
+                </div>
+                <div className="container">
+                <Image src={aS} alt="card" height={320} width={220} layout="fixed"loader={myLoader} />
+                </div>
             </div>
         </div>
     )
---
+++ pages/src/helpers/cards.js
@@ -0,0 +1,92 @@
+import aS from "../../public/images/cards/AS.svg"
+import aC from "../../public/images/cards/AC.svg"
+import aH from "../../public/images/cards/AH.svg"
+import aD from "../../public/images/cards/AD.svg"
+
+import twoS from "../../public/images/cards/Atlas_deck_2_of_spades.svg"
+import twoC from "../../public/images/cards/Atlas_deck_2_of_clubs.svg"
+import twoH from "../../public/images/cards/Atlas_deck_2_of_spades.svg"
+import twoD from "../../public/images/cards/Atlas_deck_2_of_diamonds.svg"
+
+
+import t3S from "../../public/images/cards/Atlas_deck_3_of_spades.svg"
+import t3C from "../../public/images/cards/Atlas_deck_3_of_clubs.svg"
+import t3H from "../../public/images/cards/Atlas_deck_3_of_spades.svg"
+import t3D from "../../public/images/cards/Atlas_deck_3_of_diamonds.svg"
+
+
+import fourS from "../../public/images/cards/Atlas_deck_4_of_spades.svg"
+import fourC from "../../public/images/cards/Atlas_deck_4_of_clubs.svg"
+import fourH from "../../public/images/cards/Atlas_deck_4_of_spades.svg"
+import fourD from "../../public/images/cards/Atlas_deck_4_of_diamonds.svg"
+
+
+import fiveS from "../../public/images/cards/Atlas_deck_5_of_spades.svg"
+import fiveC from "../../public/images/cards/Atlas_deck_5_of_clubs.svg"
+import fiveH from "../../public/images/cards/Atlas_deck_5_of_spades.svg"
+import fiveD from "../../public/images/cards/Atlas_deck_5_of_diamonds.svg"
+
+
+import sixS from "../../public/images/cards/Atlas_deck_6_of_spades.svg"
+import sixC from "../../public/images/cards/Atlas_deck_6_of_clubs.svg"
+import sixH from "../../public/images/cards/Atlas_deck_6_of_spades.svg"
+import sixD from "../../public/images/cards/Atlas_deck_6_of_diamonds.svg"
+
+
+import sevenS from "../../public/images/cards/Atlas_deck_7_of_spades.svg"
+import sevenC from "../../public/images/cards/Atlas_deck_7_of_clubs.svg"
+import sevenH from "../../public/images/cards/Atlas_deck_7_of_spades.svg"
+import sevenD from "../../public/images/cards/Atlas_deck_7_of_diamonds.svg"
+
+
+import heightS from "../../public/images/cards/Atlas_deck_8_of_spades.svg"
+import heightC from "../../public/images/cards/Atlas_deck_8_of_clubs.svg"
+import heightH from "../../public/images/cards/Atlas_deck_8_of_spades.svg"
+import heightD from "../../public/images/cards/Atlas_deck_8_of_diamonds.svg"
+
+
+import nineS from "../../public/images/cards/Atlas_deck_9_of_spades.svg"
+import nineC from "../../public/images/cards/Atlas_deck_9_of_clubs.svg"
+import nineH from "../../public/images/cards/Atlas_deck_9_of_spades.svg"
+import nineD from "../../public/images/cards/Atlas_deck_9_of_diamonds.svg"
+
+
+import tenS from "../../public/images/cards/Atlas_deck_10_of_spades.svg"
+import tenC from "../../public/images/cards/Atlas_deck_10_of_clubs.svg"
+import tenH from "../../public/images/cards/Atlas_deck_10_of_spades.svg"
+import tenD from "../../public/images/cards/Atlas_deck_10_of_diamonds.svg"
+
+import kS from "../../public/images/cards/Atlas_deck_king_of_spades.svg"
+import kC from "../../public/images/cards/Atlas_deck_king_of_clubs.svg"
+import kH from "../../public/images/cards/Atlas_deck_king_of_spades.svg"
+import kD from "../../public/images/cards/Atlas_deck_king_of_diamonds.svg"
+
+import qS from "../../public/images/cards/Atlas_deck_queen_of_spades.svg"
+import qC from "../../public/images/cards/Atlas_deck_queen_of_clubs.svg"
+import qH from "../../public/images/cards/Atlas_deck_queen_of_spades.svg"
+import qD from "../../public/images/cards/Atlas_deck_queen_of_diamonds.svg"
+
+import jS from "../../public/images/cards/JC.svg"
+import jC from "../../public/images/cards/JC.svg"
+import jH from "../../public/images/cards/JC.svg"
+import jD from "../../public/images/cards/JC.svg"
+
+import deckBack from "../../public/images/cards/deckBack.svg"
+
+
+export {
+    aC,aD,aH,aS,
+    twoC,twoD,twoH,twoS,
+    t3C,t3D,t3H,t3S,
+    fourC,fourD,fourH,fourS,
+    fiveC,fiveD,fiveH,fiveS,
+    sixC,sixD,sixH,sixS,
+    sevenC,sevenD,sevenH,sevenS,
+    heightC,heightD,heightH,heightS,
+    nineC,nineD,nineH,nineS,
+    tenC,tenD,tenH,tenS,
+    kC,kD,kH,kS,
+    qC,qD,qH,qS,
+    jC,jD,jH,jS,
+    deckBack
+}   
